570bbbded7a48aad55aad54853cfa934dece3345
Stutikanta Sahoo <stutikantasahoo01@gmail.com>
2026-01-17 00:25:00 +0530

    Save the changes

--- Lec10/ui_project/src/components/section1/herotext.jsx
+++ Lec10/ui_project/src/components/section1/herotext.jsx
@@ -6,7 +6,7 @@ const herotext = () => {
         <h3 className='text-7xl font-bold mb-7'>Prospective <br /><span className='text-gray-600 '>customer</span> <br />  segmentation </h3>
         <p className='text-xl w-[90%] font-medium text-gray-600'>
           Depending on customer satisfaction and access to banking
-          products,potential target audience can be divided into three groups
+          product,potential target audience can be divided into three groups
         </p>
       </div>
   )
