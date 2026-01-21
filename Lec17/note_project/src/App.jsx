e4ec2e2964ecf7f55252e0dcb43b7c02e315c7fc
Stutikanta Sahoo <stutikantasahoo01@gmail.com>
2026-01-22 00:24:23 +0530

    Save the changes

--- Lec17/note_project/src/App.jsx
+++ Lec17/note_project/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
       <form className=" flex items-center gap-5 m-10 p-10 bg-blue-100 rounded-3xl w-fit" >
         <input type="text" placeholder="Enter note heading"  className='border-2 p-5'/>
         <textarea
-          placeholder="Enter the note here"
+          placeholder="Enter note here"
          className='border-2 p-10'
         ></textarea>
       </form>
